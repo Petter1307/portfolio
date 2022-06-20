24e583319dbbc89fab37fab5b3cc715277373f9f
Petter1307 <petrealupu75@gmail.com>
2022-06-20 10:18:20 +0300

    Basic style

--- src/Components/constants/sideBar.jsx
+++ src/Components/constants/sideBar.jsx
@@ -1,5 +1,48 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link, NavLink } from "react-router-dom";
+// import { logo } from "../../assets/logo.svg";
+import {
+  faFacebook,
+  faLinkedin,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
+import { faHome, faUser, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import "./style.scss";
 const SideBar = () => {
-  return <div className="nav-bar"></div>;
+  return (
+    <div className="nav-bar">
+      {/* <img src={logo} /> */}
+      <nav>
+        <a>
+          <FontAwesomeIcon icon={faHome} />
+        </a>
+        <a>
+          <FontAwesomeIcon icon={faUser} />
+        </a>
+        <a>
+          <FontAwesomeIcon icon={faEnvelope} />
+        </a>
+      </nav>
+
+      <ul>
+        <li>
+          <a>
+            <FontAwesomeIcon icon={faFacebook} />
+          </a>
+        </li>
+        <li>
+          <a>
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+        </li>
+        <li>
+          <a>
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default SideBar;
